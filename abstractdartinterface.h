deb06bbdbe3175f50d1e680182af0155e42c7758
Martin Hansen <havnetrold2002@yahoo.dk>
2020-05-29 21:33:57 +0200

    Implementing dart interface

--- abstractdartinterface.h
+++ abstractdartinterface.h
@@ -22,16 +22,28 @@ public:
      *  -
      */
 
-    Q_INVOKABLE QString tournamentTitleFromIndex(int index);
+    virtual Q_INVOKABLE int tournamentsCount() = 0;
+    virtual Q_INVOKABLE QString tournamentIDFromIndex(const int &index) = 0;
+    virtual Q_INVOKABLE int tournamentMaxPlayers(const QString &id) = 0;
+    virtual Q_INVOKABLE int tournamentLegsCount(const QString &id) = 0;
+    virtual Q_INVOKABLE int tournamentPlayersCount(const QString &id) = 0;
 
-    /*
-     * Player related stuff
-     */
+    virtual Q_INVOKABLE QString assignedPlayerIDfromIndex(const int &tournamentID, const int &index) = 0;
+    virtual Q_INVOKABLE QString playerIDFromIndex(const int &index) = 0;
 
+    virtual Q_INVOKABLE int playerFirstName(const QString &player) = 0;
+    virtual Q_INVOKABLE int playerLastName(const QString &player) = 0;
+    virtual Q_INVOKABLE int playerEmail(const QString &player) = 0;
 
+    virtual Q_INVOKABLE int currentGameRoundIndex(const QString &tournament) = 0;
+    virtual Q_INVOKABLE int currentGameSetIndex(const QString &tournament) = 0;
 
+    virtual Q_INVOKABLE void addPoint(const int& value) = 0;
 
+    virtual Q_INVOKABLE void startGame() = 0;
+    virtual Q_INVOKABLE void stopGame() = 0;
 
+    virtual Q_INVOKABLE int score(const QString &tournament, const QString &player) = 0;
 };
 
 #endif // PROJECTDARTINTERFACE_H
