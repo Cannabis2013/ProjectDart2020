53f724d1572e48f1c9dc74dd3666919383c02318
MH2012 <MH2012@outlook.dk>
2025-08-22 22:53:18 +0200

    Removed space

--- qml/pages/tournament/scoreDisplay/scoreScripts.js
+++ qml/pages/tournament/scoreDisplay/scoreScripts.js
@@ -13,7 +13,6 @@ function updateScores(inputs, inputsScore) {
   let text = ""
   inputs.forEach(input => text += `${input.modId}${input.point} `)
   inputsBox.value = text
-
   scoreBox.value = inputsScore
 }
 
